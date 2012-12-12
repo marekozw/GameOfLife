1aa8360341d92a81876c24a9341a9bc3a3b1360d
marekozw <marek.ozw@gmail.com>
2012-12-12 20:32:54 +0100

    Refactored StateTest.java junit

--- test/com/mobica/life/StateTest.java
+++ test/com/mobica/life/StateTest.java
@@ -3,10 +3,9 @@
  */
 package com.mobica.life;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
